ba7c9e4666920616698011a27cbbdd1ac1f11aa9
RektyRowdyy <rektyrowdy@gmail.com>
2023-12-25 23:27:54 +0530

    configured API

--- LMS-API/LMS.API/LMS.API/obj/Debug/net8.0/LMS.API.AssemblyInfo.cs
+++ LMS-API/LMS.API/LMS.API/obj/Debug/net8.0/LMS.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LMS.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40ad76690353f8df9926f3a6e2236129daa49fa3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("LMS.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LMS.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
